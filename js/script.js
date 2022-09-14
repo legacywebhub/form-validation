aa45f8e297f9e5ef18e7a53393125de2727e9a3a
legacywebhub <legacywebhub@gmail.com>
2022-09-15 00:07:29 +0100

    Completed commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //Variable Declarations
+
 const myForm = document.forms["form"],
     userName = myForm["username"],
     phoneNumber = myForm["number"],
@@ -8,18 +9,25 @@ const myForm = document.forms["form"],
     submit = myForm["submit"],
     errorMessage = document.querySelector("#error-msg");
 
+
 //EventListeners
+
 myForm.addEventListener('submit', function(event){
     event.preventDefault();
-    console.log('clicked');
     validateForm();
 });
 
-//console.log()
+
 
 //Functions
+
+// This function validates our form
 function validateForm() {
-    if (password1.value != password2.value) {
+    beforeValidate();
+
+    if (password1.value.length < 7 || password2.value.length < 7) {
+        setError(password1, "Password cannot have less than 7 digits");
+    } else if (password1.value != password2.value) {
         setError(password2, "Passwords do not match");
     } else {
         if (userName.value === " ") {
@@ -34,37 +42,50 @@ function validateForm() {
            setError(phoneNumber, "Phone cannot have spaces");
         } else if (phoneNumber.value.length >= 15) {
             setError(phoneNumber, "Phone cannot have more than 12 characters");
+        } else if (isNaN(phoneNumber.value)) {
+            setError(phoneNumber, "Phone cannot have letters or symbols");
         } else {
-            console.log(checkRadio());
-            if (checkRadio() == false) {
+            let checked, gender;
+
+            if (document.getElementById('male_gender').checked) {
+                gender = document.getElementById('male_gender').value;
+                checked = true;
+            } else if (document.getElementById('female_gender').checked) {
+                gender = document.getElementById('female_gender').value;
+                checked = true;
+            } else {
+                checked = false;
+            }
+
+            if (checked == false) {
                 errorMessage.innerText = "Please choose a gender";
             } else {
-                console.log("Sign up was successful!");
+                errorMessage.innerText = "Successfully validated!";
+                errorMessage.style.color = "rgba(0, 231, 0, 0.80)";
+                alert(
+                `Validation successful!
+
+                username: ${userName.value}, phone number: ${phoneNumber.value},
+                gender: ${gender}, password: ${password2.value}`
+                )
             }
         }
     }
 }
 
-function checkRadio() {
-    let checked;
-
-    radio.forEach((gender) => {
-        console.log(gender.unchecked);
-
-        if (gender.unchecked) {
-            checked = false;
-        } else {
-            checked = true;
-        }
-        return checked
-    })
+// This functions resets our DOM for new validation
+function beforeValidate() {
+    errorMessage.style.color = "rgba(255, 0, 0, 0.9)";
+    const errorIcons = document.querySelectorAll(".fa-exclamation-circle");
+    errorIcons.forEach((icon)=>{icon.style.visibility = "hidden"});
 }
 
-
+// This function make errors changes to the DOM
 function setError(input, message) {
     const inputParent = input.parentElement;
-    const inputIcon = inputParent.querySelector(".fa-exclamation-circle");
-    inputIcon.style.visibility = "visible";
+    const errorIcon = inputParent.querySelector(".fa-exclamation-circle");
+    errorIcon.style.visibility = "visible";
     errorMessage.innerText = message;
     errorMessage.style.padding = "0.5rem 1rem";
+    errorMessage.style.color = "rgba(255, 0, 0, 0.9)";
 }
